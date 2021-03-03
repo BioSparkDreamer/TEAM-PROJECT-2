d7a61cd7870c98ccbd57bb6b7623ae5908b12387
BentNai1 <eattopramen@hotmail.com>
2021-03-03 16:52:29 -0500

    Initial 3d player movement script
    I am way less comfortable in 3d than I am in 2d, but it works well enough!

---
+++ TEAM PROJECT 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // ...............................................Movement Variables
+    private Rigidbody rigidBody;
+    private Vector3 playerVelocity;
+    public float playerSpeed = 2.0f;
+    
+    float horizontal;
+    float vertical;
+
+
+    void Start()
+    {
+        // ...........................................Instantiate Movement
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //...........................................Movement Detection
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        //...........................................Movement Action
+        rigidBody.velocity = new Vector3(horizontal * playerSpeed, rigidBody.velocity.y, vertical * playerSpeed);
+        print(horizontal);
+    }
+}
