57797fe1d0564e2a74a114553b00a7a22efdbb73
BentNai1 <eattopramen@hotmail.com>
2021-03-08 18:09:26 -0500

    Jump Script Implemented
    Fully customizable jump script is live - change gravity and jump strength straight in the inspector to match the needs of the level design. I also separated the rune code into it's own script to prevent possible Git conflicts in the future.
    
    A note about the jump script: The ground checker that is a part of it requires that the surface the player is standing on be a part of the "Ground" mask. I changed the planes in all of the scenes to be "Ground", but any future surfaces you want the player to walk on will need the appropriate layer mask as well.

--- TEAM PROJECT 2/Assets/Scripts/PlayerMovement.cs
+++ TEAM PROJECT 2/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
+// =================NOTE=====================
+// The ground checker only looks at objects tagged
+// with the "Ground" layer (to keep it from checking 
+// itself). Any terrain you want the player to walk
+// on should have the "Ground" layer on them.
 
 public class PlayerMovement : MonoBehaviour
 {
     //..............................................Variables
     public CharacterController controller;
+
     public float moveSpeed = 10f;
 
-    public int runes = 0;
+    public float gravity = -9.81f;
+    Vector3 fallVelocity;
+
+    public Transform groundCheck;
+    public float groundCheckDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public float jumpStrength = 3f;
 
     void Update()
     {
+        //..............................................Ground check/Fall velocity reset
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, groundMask);
+        //resets fall velocity to a small number if close
+        if(isGrounded && fallVelocity.y < 0)
+        {
+            fallVelocity.y = -2f;
+        }
         //..............................................Get input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -24,10 +45,16 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(newLocation * moveSpeed * Time.deltaTime);
 
-        //..............................................Conditional to get into temple
-        if(runes == 3)
+        //..............................................Jump
+        if(Input.GetButtonDown("Jump") && isGrounded)
         {
-            SceneManager.LoadScene(1);
+            fallVelocity.y = Mathf.Sqrt(jumpStrength * -2f * gravity);
         }
+
+        //..............................................Player fall velocity
+        //Note: multiplied by Time.deltaTime twice 
+        //because that's just how physics works man
+        fallVelocity.y = fallVelocity.y + gravity * Time.deltaTime;
+        controller.Move(fallVelocity * Time.deltaTime);
     }
 }
