ac324031f64910fbfe8aadef6ff98cf50dd69e00
MaxwellBustamante <59800680+MaxwellBustamante@users.noreply.github.com>
2021-03-20 00:19:31 -0400

    Death/Win Screen Fixed

---
+++ TEAM PROJECT 2/Assets/Scripts/SetCursorStateOnStart.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+/// <summary>
+/// Place this on an empty object in scenes that do not contain <see cref="MouseLook"/>
+/// This will set the initial cursor lockstate to <see cref="_cursorModeOnStart"/>
+/// </summary>
+public class SetCursorStateOnStart : MonoBehaviour {
+	public CursorLockMode _cursorModeOnStart;
+	public void Start() {
+		Cursor.lockState = _cursorModeOnStart;
+	}
+}
